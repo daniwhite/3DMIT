a9d3407e1c56f1c3e1989e98e7628fbf8f2a16f5
Dani White <white.dani.m@gmail.com>
2017-09-16 19:13:58 -0400

    Reformat

--- opencvtest.py
+++ opencvtest.py
@@ -1,31 +1,44 @@
 import cv2
-img = cv2.imread('../1_3.png')
 
+default_filepath = '../1_3.png'
 MIN_AREA = 500  # Could be tuned
+MAX_AREA = 20000  # Could be tuned
 
-# Make all pixels that aren't pure white into black pixels
-ret, img2 = cv2.threshold(img, 254, 255, cv2.THRESH_TOZERO)
-# Ensure the image is black and white for finding contours
-img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+def find_room_contours(min_area=MIN_AREA, max_area=MAX_AREA, path=default_filepath):
+    img = cv2.imread(path)
 
+    # Make all pixels that aren't pure white into black pixels
+    ret, img2 = cv2.threshold(img, 254, 255, cv2.THRESH_TOZERO)
+    # Ensure the image is black and white for finding contours
+    img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 
-img2, contours, hierarchy = cv2.findContours(img2, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    img2, contours, hierarchy = cv2.findContours(img2, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
-# Array of colors to cycle through
-colors = [(0, 255, 0), (0, 255, 255), (255, 0, 0), (0, 0, 255), (255, 255, 0), (255, 0, 255)]
-good_contours = []
+    good_contours = []
 
-# Throw out contours that are too small
-for cnt in contours:
-    if cv2.contourArea(cnt) > MIN_AREA:
-        good_contours.append(cnt)
+    # Throw out contours that are too small
+    for cnt in contours:
+        if cv2.contourArea(cnt) > min_area and cv2.contourArea(cnt) < max_area:
+            good_contours.append(cnt)
+    return good_contours
 
-# Draw contours in different colors
-for i in range(len(good_contours)):
-    cnt = good_contours[i]
-    img2 = cv2.drawContours(img, [cnt], -1, colors[i % len(colors)], 1)
+if __name__ == "__main__":
+    min_area = int(input("Min area: "))
+    max_area = int(input("Max area: "))
+    path = input("Filepath: ")
+    contours = find_room_contours(min_area, max_area, path)
 
-# Show image
-cv2.imshow('image', img2)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    # Array of colors to cycle through
+    colors = [(0, 255, 0), (0, 255, 255), (255, 0, 0), (0, 0, 255), (255, 255, 0), (255, 0, 255)]
+
+    img = cv2.imread(path)
+
+    # Draw contours in different colors
+    for i in range(len(contours)):
+        cnt = contours[i]
+        img2 = cv2.drawContours(img, [cnt], -1, colors[i % len(colors)], 1)
+
+    # Show image
+    cv2.imshow('image', img2)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
