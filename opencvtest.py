f580342810e5b4cafd23f1f1bf02548da7d3730c
Dani White <white.dani.m@gmail.com>
2017-09-16 19:00:41 -0400

    Add comments/clean up

--- opencvtest.py
+++ opencvtest.py
@@ -1,22 +1,32 @@
 import cv2
 img = cv2.imread('../1_3.png')
 
-MIN_AREA = 500
+MIN_AREA = 500  # Could be tuned
+MAX_AREA
 
-ret,img2 = cv2.threshold(img,254,255,cv2.THRESH_TOZERO)
+# Make all pixels that aren't pure white into black pixels
+ret, img2 = cv2.threshold(img, 254, 255, cv2.THRESH_TOZERO)
+# Ensure the image is black and white for finding contours
 img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 
+
 img2, contours, hierarchy = cv2.findContours(img2, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-print(len(contours))
+
+# Array of colors to cycle through
 colors = [(0, 255, 0), (0, 255, 255), (255, 0, 0), (0, 0, 255), (255, 255, 0), (255, 0, 255)]
 good_contours = []
-for i in contours:
-    if cv2.contourArea(i) > MIN_AREA:
-        good_contours.append(i)
+
+# Throw out contours that are too small
+for cnt in zipcontours:
+    if cv2.contourArea(cnt) > MIN_AREA:
+        good_contours.append(cnt)
+
+# Draw contours in different colors
 for i in range(len(good_contours)):
     cnt = good_contours[i]
     img2 = cv2.drawContours(img, [cnt], -1, colors[i % len(colors)], 1)
 
+# Show image
 cv2.imshow('image', img2)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
