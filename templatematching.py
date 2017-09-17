d85dae477da3004d718ae015e4721c4271709b0f
Dani White <white.dani.m@gmail.com>
2017-09-16 21:15:51 -0400

    Detect multiple doors

--- templatematching.py
+++ templatematching.py
@@ -1,9 +1,10 @@
 import cv2
+import numpy as np
 
 default_filepath = '../1_3.png'
 template_path = 'door_down.png'
 
-def match_template(img_path, template_path):
+def match_template(img_path, template_path, threshold=8*10**5):
     img_color = cv2.imread(img_path)
     img = cv2.imread(img_path, 0)
     temp = cv2.imread(template_path, 0)
@@ -11,11 +12,9 @@ def match_template(img_path, template_path):
     w, h = temp.shape[::-1]
 
     result = cv2.matchTemplate(img, temp, cv2.TM_CCOEFF)
-    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
-
-    bottom_right = max_loc[0] + w, max_loc[1] + h
-
-    cv2.rectangle(img_color, max_loc, bottom_right, (255, 255, 0), 2)
+    loc = np.where(result >= threshold)
+    for pt in zip(*loc[::-1]):
+        cv2.rectangle(img_color, pt, (pt[0]+w, pt[1]+h), (255, 255, 0), 2)
 
     # Show image
     cv2.imshow('image', img_color)
