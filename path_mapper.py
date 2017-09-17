aefd33fc0767ef595ec79d8eb9f0de20a1704ba1
Jenna Himawan <jenna.himawan@gmail.com>
2017-09-17 10:09:05 -0400

    Update path_mapper.py. CURRETNLY DOES NOT WORK.

--- path_mapper.py
+++ path_mapper.py
@@ -8,10 +8,16 @@ Created on Sat Sep 16 21:26:14 2017
 import networkx as nx
 import matplotlib.pyplot as plt
 import processfloorplans
+import cv2
 import random
 
 
 def mapPath(start, end, img):
+    '''Start and end are strings (names of nodes).
+    If start or end has value 'random', a random node gets chosen.
+    img is a filepath.'''
+    img = cv2.imread(img, 0)
+    
     contours = processfloorplans.find_room_contours(img)
     nodes = processfloorplans.compute_vertices(contours)
     edges = processfloorplans.compute_edges(img)
@@ -37,6 +43,8 @@ def mapPath(start, end, img):
 
     return (nx.shortest_path(G, start, end),
            nx.shortest_path_length(G, start, end))
+
+
 
 mapPath('random', 'random',
         '/Users/jennahimawan/Desktop/1_0.png')
