c33cbde430b2f0953b0d033012d8682b28b9e9e3
Jenna Himawan <jenna.himawan@gmail.com>
2017-09-17 10:03:43 -0400

    Connect path_mapper.py to processfloorplans.py. CURRENTLY NOT WORKING

--- path_mapper.py
+++ path_mapper.py
@@ -7,17 +7,24 @@ Created on Sat Sep 16 21:26:14 2017
 
 import networkx as nx
 import matplotlib.pyplot as plt
+import processfloorplans
+import random
 
-def mapPath(start, end, nodes = ["hall1", "hall2", "100", "101", "102"],
-        edges = [("hall1", "100", {'weight':1}), 
-             ("hall1", "101", {'weight':2}),
-             ("100", "101", {'weight':1}),
-             ("hall1", "hall2", {'weight':3}),
-             ("hall2", "102", {'weight':1})]):
+
+def mapPath(start, end, img):
+    contours = processfloorplans.find_room_contours(img)
+    nodes = processfloorplans.compute_vertices(contours)
+    edges = processfloorplans.compute_edges(img)
+    if start == 'random':
+        start = random.choice(nodes)
+    if end == 'random':
+        end = random.choice(nodes)
+    
+    
     G=nx.Graph()
     
     G.add_nodes_from(nodes)
-    edges = 
+##    edges =
     G.add_edges_from(edges)
     
     print(nx.shortest_path(G, start, end))
@@ -30,3 +37,6 @@ def mapPath(start, end, nodes = ["hall1", "hall2", "100", "101", "102"],
 
     return (nx.shortest_path(G, start, end),
            nx.shortest_path_length(G, start, end))
+
+mapPath('random', 'random',
+        '/Users/jennahimawan/Desktop/1_0.png')
